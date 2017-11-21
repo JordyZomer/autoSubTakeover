fdd7b6fdd2bd2a44723122ec36da612c7d13970e
Richard Clifford <darkmantis@cybershade.org>
2017-11-21 11:13:44 +0000

    Updated main.py to clean the code a bit.
    Also added the option to use custom DNS resolvers.

--- main.py
+++ main.py
@@ -22,68 +22,97 @@ If the CNAME resolves to a non-scope adress it might be worth checking out if su
 import inspect, os
 import argparse
 import dns.resolver
+import threading
 
-# External imports 
+# External imports
 from subbrute import subbrute
 import whois
 from datetime import datetime
 
-# Function to grab expiry date from the domain
-# and return whether it is expired or not
-def w(domain):
-	stripdomain = domain.rstrip('.')
-  	fixdomain = "{:s}".format(stripdomain)
-	now = datetime.now()
-	info = whois.whois('{:s}'.format(fixdomain))
-	if info.expiration_date[0] < now:
-		return "\033[91mVULN (%s)\033[0m" % info.expiration_date[0]
-	else:
-		return "\033[93mNOTVULN (%s)\033[0m" % info.expiration_date[0] 
+class AutoSubTakeOver:
+	domains = []
+	domain_resolvers = []
+
+
+	# Function to grab expiry date from the domain
+	# and return whether it is expired or not
+	def is_expired(self, domain):
+		stripdomain = domain.rstrip('.')
+		fixdomain = "{:s}".format(stripdomain)
+		now = datetime.now()
+		info = whois.whois('{:s}'.format(fixdomain))
+		if info.expiration_date[0] < now:
+			return "\033[91mVULN (%s)\033[0m" % info.expiration_date[0]
+		else:
+			return "\033[93mNOTVULN (%s)\033[0m" % info.expiration_date[0]
 		
 
-# Load argument parsing
-parser = argparse.ArgumentParser(description='Welcome to AutoSubTakeover by  Oblivion, Mantis, Zoidberg')
-parser.add_argument('--target-file', type=str, help='The file that contains the hosts')
-parser.add_argument('--output-file', type=str, help='The file that will contain the output')
-args = parser.parse_args()
+	def check(self):
+		# Initialize Resolver and use Google DNS servers
+		dnsResolver = dns.resolver.Resolver()
+		dnsResolver.nameservers = self.domain_resolvers
 
-# The file with the targets we want is behind the parameter --target-file
-target_file = args.target_file
-output_file = args.output_file
+		for domain in self.domains:
 
-# Open the target file (Read-Only) as targets  and read em :D 
-domains = open(target_file).read().split('\n')
-# Initialize Resolver and use Google DNS servers
-dnsResolver = dns.resolver.Resolver()
-dnsResolver.nameservers = ['8.8.8.8', '8.8.4.4']
+			domain = "{:s}".format(domain)
 
-output_file_handle = open(output_file, 'w', 0)
+			expired = self.is_expired(domain)
 
-# For every domain in target_file do
-for domain in domains:
-  domain = "{:s}".format(domain)
-  expired = w(domain)
-# For every entry in Subbrute's output check if it's a CNAME and if it resolves to scope
-  for entry in subbrute.run(domain):
-    	try:
-        # Query the DNS resolver to check if subdomain is a CNAME
-   	     answers = dnsResolver.query(entry[0], 'CNAME')
-	# Query whois
- 	     for rdata in answers:
-             	output = '{:s}'.format(rdata.target)
-   	      	  # If scope is/not in output splash some crap out
-                if domain in output:
-                    output_text = "[%s] - Expired: [%s] - CNAME: %s \033[93mresolves\033[0m to scope" % (entry[0], expired, output)
-                    output_file_handle.write(output_text + "\n")
-                    print output_text
-                else:
-                    output_text = "[%s] - Expired: [%s] - CNAME %s \033[91mdoes not\033[0m resolve to scope" % (entry[0], expired, output)
-                    output_file_handle.write(output_text + "\n")
-                    print output_text
-	# To solve those "BLAH SUBDOMAIN IS NO CNAME" errors
-	except dns.resolver.NoAnswer:
-   	     pass
-	except dns.resolver.NXDOMAIN:
-	     pass
+			# For every entry in Subbrute's output check if it's a CNAME and if it resolves to scope
+			for entry in subbrute.run(domain):
 
-output_file_handle.close()
+				try:
+					# Query the DNS resolver to check if subdomain is a CNAME
+					answers = dnsResolver.query(entry[0], 'CNAME')
+					
+					# Query whois
+					for rdata in answers:
+				
+						output = '{:s}'.format(rdata.target)
+				
+						output_file_handle = open(output_file, 'w', 0)
+				
+						# If scope is/not in output splash some crap out
+				
+						if domain in output:
+							output_text = "[%s] - Expired: [%s] - CNAME: %s \033[93mresolves\033[0m to scope" % (entry[0], expired, output)
+							print output_text
+							output_file_handle.write(output_text + "\n")
+				
+						else:
+							output_text = "[%s] - Expired: [%s] - CNAME %s \033[91mdoes not\033[0m resolve to scope" % (entry[0], expired, output)
+							# This should be returned
+							print output_text
+							output_file_handle.write(output_text + "\n")
+
+						output_file_handle.close()
+				# To solve those "BLAH SUBDOMAIN IS NO CNAME" errors
+				except dns.resolver.NoAnswer:
+					pass
+				except dns.resolver.NXDOMAIN:
+					pass
+				finally:
+					return output_text
+
+def main():
+
+	# Load argument parsing
+	parser = argparse.ArgumentParser(description='Welcome to AutoSubTakeover by  Oblivion, Mantis, Zoidberg')
+	parser.add_argument('--target-file', type=str, required=True, help='The file that contains the hosts')
+	parser.add_argument('--output-file', type=str, required=True, help='The file that will contain the output')
+	parser.add_argument('--dns', type=str, default="8.8.8.8,8.8.4.4", help='The number of threads to run on, default=1')
+	args = parser.parse_args()
+
+	# The file with the targets we want is behind the parameter --target-file
+	target_file = args.target_file
+	output_file = args.output_file
+
+	ast = AutoSubTakeOver()
+
+	# Open the target file (Read-Only) as targets  and read em :D
+	ast.domains = open(target_file).read().split('\n')
+	ast.domain_resolvers = args.dns.split(',')
+	ast.check()
+
+if __name__ == '__main__':
+	main()
